934ab91683a5edf26fa16117947656f156b37749
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-04 19:21:25 -0700

    BinaryConverter.java-src/extra

--- src/extra/BinaryConverter.java
+++ src/extra/BinaryConverter.java
@@ -2,6 +2,8 @@ package extra;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,48 +11,57 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class BinaryConverter implements ActionListener {
-public static void main(String[] args) {
-	JFrame frame = new JFrame(); 
+	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-    JTextField answer = new JTextField(20);
+	JTextField answer = new JTextField(20);
 	JButton Button = new JButton();
-  
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.add(panel);
-    panel.add(answer);
-    panel.add(Button);
-    Button.addMouseListener(null);
-	frame.setVisible(true);
-   
 
-	
-	
-}
+	BinaryConverter() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(answer);
+		panel.add(Button);
+		Button.addActionListener(this);
+		frame.setVisible(true);
+		Button.setText("Convert");
+		frame.pack();
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-String convert(String answer) {
-    if(answer.length() != 8){
-         JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-         return "-";
-    }
-    String binary = "[0-1]+";    //must contain numbers in the given range
-    if (!answer.matches(binary)) {
-         JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-         return "-";
-    }
-    try {
-         int asciiValue = Integer.parseInt(answer, 2);
-         char theLetter = (char) asciiValue;
-         return "" + theLetter;
-    } catch (Exception e) {
-         JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-         return "-";
-    }
-}
+	}
+
+	public static void main(String[] args) {
+		BinaryConverter bc = new BinaryConverter();
+	}
+
+	String convert(String input) {
+        if(input.length() != 8){
+             JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+             return "-";
+        }
+        String binary = "[0-1]+";    //must contain numbers in the given range
+        if (!input.matches(binary)) {
+             JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+             return "-";
+        }
+        try {
+             int asciiValue = Integer.parseInt(input, 2);
+             char theLetter = (char) asciiValue;
+             return "" + theLetter;
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+             return "-";
+        }
+   }
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) arg0.getSource();
+
+		if (buttonPressed == Button) {
+		JOptionPane.showMessageDialog(null, (convert(answer.getText())) );	
+		
+			
+		}
+
+	}
 }
